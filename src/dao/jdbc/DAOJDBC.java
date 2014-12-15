3f42cbc2ab8fb4a292e48e71c12777bae8e3371d
PERLINSKI Eric <eric.perlinski@telecomnancy.net>
2014-12-15 12:04:44 +0100

    Debut creation model + classe de connection à la bdd

---
+++ src/dao/jdbc/DAOJDBC.java
@@ -0,0 +1,9 @@
+package dao.jdbc;
+
+public class DAOJDBC {
+	
+	final private static String DRIVER="com.mysql.jdbc.Driver";
+	final private static String URL="jdbc:mysql://webpanel.telecomnancy.univ-lorraine.fr/alternc-sql/codingweek_prj11";
+	final private static String USER="codingweek_prj11";
+	final private static String MDP="i1FpwTi0a";
+}
---
+++ src/model/WebPage.java
@@ -0,0 +1,67 @@
+package model;
+
+public class WebPage {
+	private int id = -1;
+	private int nblike;
+	private int id_admin = -1;
+	private String lien;
+	
+	public WebPage() {
+		super();
+	}
+	
+	public WebPage(int newId,int newNblike,int newId_admin,String newLien){
+		this.id = newId;
+		this.nblike = newNblike;
+		this.id_admin = newId_admin;
+		this.lien = newLien;
+	}
+	
+	public WebPage(int newId){
+		this.id = newId;
+		this.nblike = 0;
+		this.id_admin = 0;
+		this.lien = "";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getNblike() {
+		return nblike;
+	}
+
+	public void setNblike(int nblike) {
+		this.nblike = nblike;
+	}
+
+	public int getId_admin() {
+		return id_admin;
+	}
+
+	public void setId_admin(int id_admin) {
+		this.id_admin = id_admin;
+	}
+
+	public String getLien() {
+		return lien;
+	}
+
+	public void setLien(String lien) {
+		this.lien = lien;
+	}
+
+	@Override
+	public String toString() {
+		return "WebPage [id=" + id + ", nblike=" + nblike + ", id_admin="
+				+ id_admin + ", lien=" + lien + "]";
+	}
+
+	
+	
+}
