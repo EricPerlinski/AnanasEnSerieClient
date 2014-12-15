af1ed0dba9283cb17bcb66ce6feb9bc6e85f59ff
PERLINSKI Eric <eric.perlinski@telecomnancy.net>
2014-12-15 12:09:42 +0100

    getConnection jdbc

--- src/dao/jdbc/DAOJDBC.java
+++ src/dao/jdbc/DAOJDBC.java
@@ -1,9 +1,29 @@
 package dao.jdbc;
 
+import java.sql.*;
+
 public class DAOJDBC {
 	
 	final private static String DRIVER="com.mysql.jdbc.Driver";
 	final private static String URL="jdbc:mysql://webpanel.telecomnancy.univ-lorraine.fr/alternc-sql/codingweek_prj11";
 	final private static String USER="codingweek_prj11";
 	final private static String MDP="i1FpwTi0a";
+	
+	public Connection getConnection() {
+			Connection connection = null;
+			
+			try{
+				Class.forName(DRIVER);
+			}catch(ClassNotFoundException e){
+				e.printStackTrace();
+			}
+			
+			try{
+				connection = DriverManager.getConnection(URL,USER,MDP);	
+			}catch(SQLException e){
+				e.printStackTrace();
+			}
+			
+			return connection;
+	}
 }
