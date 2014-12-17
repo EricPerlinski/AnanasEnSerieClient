6f19738c91621798d70c2fdb0c25284eeb8841fb
Valentin Jeannot <valentin.jeannot@telecomnancy.net>
2014-12-17 16:34:22 +0100

    Vue RadioButtonQuestion

--- src/view/SurveyJPanel.java
+++ src/view/SurveyJPanel.java
@@ -1,9 +1,16 @@
 package view;
 
+import javax.swing.JList;
 import javax.swing.JPanel;
 
 public class SurveyJPanel  extends JPanel{
 
 	private static final long serialVersionUID = -584049310533560753L;
+	
+	private JList list;
+	
+	public void addList(JPanel jp){
+		list.add(jp);
+	}
 
 }
